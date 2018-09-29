68cb8ec638ef1d092d8ad8e211b8150da163df1e
mmengote <ana@marianamengote.com>
2018-09-29 12:40:29 -0400

    Update master.py

--- master.py
+++ master.py
@@ -1,1 +1,25 @@
 #testing
+class Animal:
+    def _init_(self, age, predator)
+    self.age = age
+    self.predator = 
+
+#class Lion(Animal):
+#class Monkey(Animal):
+#class Penguin(Animal):
+#class Bear(Animal):
+#class Sheep(Animal):
+#class Deer(Animal):
+#class Leopard(Animal):
+#class Sloth(Animal):
+#class Bird(Animal):
+#class Otter(Animal):
+#class Pandas(Animal):
+
+class LocalAnimal:
+    def _init_(self, age)
+    
+#class Human(LocalAnimal):
+#class Squirrel(LocalAnimal):
+#class Pigeons(LocalAnimal):
+#class Raccoon(LocalAnimal):
