e393ab32133414a95d1b73141d9db0624947c720
Dolly Setton <drs22@columbia.edu>
2018-10-02 19:46:49 -0400

    revised animal class

--- animalclass.py
+++ animalclass.py
@@ -1,24 +1,28 @@
-class Bear():
+class Animal():
     
-    def __init__(self, age, bearid, howbig, knowledge, energy):
+    def __init__(self, animalid, species, age, howbig, knowledge, energy):
        self.age = age
-       self.bearid = bearid
-       #self.color= color
+       self.animalid = animalid
+       self.species = species
        self.howbig= howbig
        self.knowledge = knowledge
        self.energy = energy
        
     def move(self):
-       print(str(self.bearid) + " is now moving.")
+       print(str(self.animalid) + " is now moving.")
        if self.energy <15:
-            print("He is moving slow!")
-       else: print("He is moving pretty fast!")
+            print(str(self.animalid) + " is moving slow!")
+            print("That " + str(self.species) + " is " + str(self.age) + " years old.")
+       else: print("That " + str(self.species) + " is moving pretty fast!")
        
-first_bear = Bear(12, 'Joe', 'large', 23, 25)
-# print("The first animal is a " + animaly.age + ".")
-# print("the first animal is" + animaly.age + " years old.")
+first_bear = Animal('bear1', 'bear', 14, 'large', 23, 25)
+#print("The first animal is a " + animaly.age + ".")
+#print("the first animal is" + animaly.age + " years old.")
 
-second_bear = Bear(13, "Mark", 'small', 12, 9)
+second_bear = Animal('bear2', "bear", 12, 'small', 12, 9)
+
+first_monkey = Animal('monkey1', "monkey", 15, 'small', 17, 9)
 
 first_bear.move()
 second_bear.move()
+first_monkey.move()
